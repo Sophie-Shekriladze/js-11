29828e05fd755f198ed70382657983fbac3ebee0
Sophie Shekriladze <55558347+Sophie-Shekriladze@users.noreply.github.com>
2023-04-02 15:09:09 +0400

    add email

--- js/form.js
+++ js/form.js
@@ -9,7 +9,6 @@ formElement.addEventListener("submit", function (event) {
   if (username == "" || username.length < 5) {
     errors.username = "username cant be empty";
   }
- 
 
   // password
   let password1 = document.getElementById("password-field1").value;
@@ -59,5 +58,50 @@ formElement.addEventListener("submit", function (event) {
     if (errorText) {
       errorText.innerText = errors[item];
     }
+  }
+
+  if (Object.keys(errors).length == 0) {
+    form.submit();
+  }
+});
+
+//show hide passsword
+
+let passswordField = document.getElementById("password-field1");
+let toggleIcon = document.getElementById("toggleIcon");
+
+toggleIcon.addEventListener("click", function () {
+  if (passswordField.type == "password") {
+    passswordField.setAttribute("type", "text");
+    toggleIcon.classList.remove("fa-eye");
+    toggleIcon.classList.add("fa-eye-slash");
+  } else {
+    passswordField.setAttribute("type", "password");
+    toggleIcon.classList.remove("fa-eye-slash");
+    toggleIcon.classList.add("fa-eye");
+  }
+});
+
+//email regex
+
+let emailField = document.getElementById("emailField");
+
+emailField.addEventListener("keyup", function () {
+  let emailFieldVAlue = document.getElementById("emailField").value;
+  let perrortext = document.getElementById("error-email");
+
+  let emailPattern =
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+  if (emailFieldVAlue.match(emailPattern)) {
+    emailField.style.border = "2px solid green";
+    emailField.style.outline = "none";
+    perrortext.innerHTML = "your email is valid";
+    perrortext.style.color = "green";
+  } else {
+    emailField.style.border = "2px solid red";
+    perrortext.innerHTML = "your email is invalid";
+    perrortext.style.color = "red";
+    emailField.style.outline = "none";
   }
 });
